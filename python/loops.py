2580b40c9ff7fccefa20d7167e2173eac43202c1
TheParadox20 <onyambusamson@gmail.com>
2022-02-14 15:27:14 -0500

    starting dart

---
+++ python/loops.py
@@ -0,0 +1,16 @@
+"""
+    while <condition>:
+        <code>
+"""
+i=0
+while i<10:
+    print('i:',i)
+    i+=1
+
+"""
+    for <var> in <iterable>:
+        <code>
+"""
+
+for char in "greater good":
+    print(char,' ',end='')
