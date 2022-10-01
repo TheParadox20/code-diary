a3c609a42b42fe452890b5ee6d6235fdf7a0514d
TheParadox20 <onyambusamson@gmail.com>
2022-10-01 14:10:07 +0300

    hello kotlin

---
+++ kotlin/numerals.kt
@@ -0,0 +1,5 @@
+fun main(args: Array<String>){
+    println("Kotlin Numerals")
+    //Kotlin keeps numbers as primitives but lets you call methods on numbers as if they were objects
+    println(2.times(5))
+}
